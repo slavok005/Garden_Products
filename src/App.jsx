aa7b5af32cb2b53a087c503e76d6a0bd3e40d039
Grigory Koltso <grifao26@gmail.com>
2024-10-10 14:52:01 +0200

    oncentroval i popravil paru melkih oshibok

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import React from "react";
 import NotFoundPage from "./pages/NotFoundPage";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Banner from "./components/MainBanner";
-import Sales from "./components/DiscountForm/index";
 import { Route, Routes } from "react-router-dom";
-import Categories from "./components/Categories";
-import DiscountForm from "./components/DiscountForm/index";
 import MainPage from "./pages/MainPage";
 import Layout from "./pages/Layout";
-
 import TotalSales from "./components/TotalSales";
-
 import AllProductsPage from './pages/AllProductsPage';
 import AllSalesPage from './pages/AllSalesPage';
 import CategoriesPage from './pages/CategoriesPage';
-
+import './index.css';
 
 
 function App() {
   return (
-    <div className="main_container">
+    <div className='main_container'>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<MainPage />} />
