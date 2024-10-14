af0a9ca070e2f1df5369fbfb74d803414ff08ed6
Grigory Koltso <grifao26@gmail.com>
2024-10-14 19:33:12 +0200

    SingleProductPage is finished

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -5,6 +5,7 @@ import Categories from '../../components/Categories'
 import DiscountForm from '../../components/DiscountForm'
 import NavigationBranch from '../../components/NavigationBranch';
 import BannerSales from '../../components/BannerSales';
+import SingleProductCard from '../../components/SingleProductCard';
 
 const MainPage = () => {
   return (
@@ -12,6 +13,7 @@ const MainPage = () => {
           {/* <NavigationBranch /> */}
           <Banner />
           <Categories />
+          <SingleProductCard/>
           <DiscountForm/>
           <BannerSales/>
     </div>
