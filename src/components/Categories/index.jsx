fb546a402ce3095fa5ee3502cb1d47576d86ff17
Slava <slavok.v005@mail.ru>
2024-10-16 20:08:47 +0200

    decidedgit add .!

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import s from './index.module.scss';
-import { Link } from 'react-router-dom';
-import CategoriesItem from '../CategoriesItem/index.jsx';
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllCategories } from '../requests/categories';
+import React, { useState, useEffect } from "react";
+import s from "./index.module.scss";
+import { Link } from "react-router-dom";
+import CategoriesItem from "../CategoriesItem/index.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCategories } from "../requests/categories";
 
 function Categories() {
   const dispatch = useDispatch();
@@ -14,12 +14,9 @@ function Categories() {
 
   const categoriesState = useSelector((store) => store.categories);
 
- const partCategories = categoriesState.slice(0, 4);
-
-const partCategories = categories.slice(0, 4);
+  const partCategories = categoriesState.slice(0, 4);
 
   return (
-    
     <div className={s.categories}>
       <div className={s.header}>
         <h2>Categories</h2>
@@ -28,30 +25,14 @@ const partCategories = categories.slice(0, 4);
           <Link to="/categories">All Categories</Link>
         </button>
       </div>
-      
+
       <div className={s.categoriesList}>
-        
         {partCategories.map((element) => (
-          
-          < CategoriesItem key={element.id} {...element} />
-            
-                   
+          <CategoriesItem key={element.id} {...element} />
         ))}
-
       </div>
-   
-
-        <div className={s.categoriesList}>
-          {partCategories.map((element) => (
-            <div key={element.id} className={s.categoryItem}>
-              <img src={`http://localhost:3333${element.image}`} alt={element.title} />
-              <p>{element.title}</p>
-            </div>
-          ))}
-        </div>     
-
+     
     </div>
-  
   );
 }
 
